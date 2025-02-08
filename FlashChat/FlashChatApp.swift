119bec5fa0094ed6df3fcf162be6a1662ff836f8
DanyloM73 <danyamalanin7@gmail.com>
2025-02-08 15:28:02 +0200

    Initial Commit

---
+++ FlashChat/FlashChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashChatApp.swift
+//  FlashChat
+//
+//  Created by danylo on 08.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct FlashChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
